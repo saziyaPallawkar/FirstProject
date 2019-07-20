0b94480c35e1e9ef4f5b76551850ae63d0e432bd
Saziya Pallawkar <shaziya_mukadam@hotmail.co.uk>
2019-07-20 16:42:53 +0100

    adding supermarket project

---
+++ supermarket/supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Checkout checkout = new Checkout();
+            checkout.addItem(new Item("A", 50, 130, 3));
+            checkout.addItem(new Item("B", 30, 45, 2));
+            checkout.addItem(new Item("C", 20, 0, 0));
+            checkout.addItem(new Item("D", 15, 0, 0));
+            string decision = "1";
+            do
+            {
+                Console.WriteLine("Please scan the item");
+                string Sku = Console.ReadLine();
+                checkout.Scan(Sku);
+                Console.WriteLine("Press 1 to scan more items or press ...");
+                decision = Console.ReadLine();
+            }
+            while (decision == "1");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Total Price is -");
+            Console.WriteLine("");
+            Console.WriteLine(checkout.GetTotal());
+        }
+    }
+}
---
+++ supermarket/supermarket/SuperMarket.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+
+public interface ICheckout
+{
+    void Scan(string item);
+    int GetTotal();
+}
+
+public class Item
+{
+    public Item(string sku, int price, int specialPrice = 0, int specialCount = 0)
+    {
+        this.Sku = sku;
+        this.Price = price;
+        this.SpecialPrice = specialPrice;
+        this.SpecialCount = specialCount;
+    }
+
+    public string Sku;
+    public int Price;
+    public int SpecialPrice;
+    public int SpecialCount;
+    public int getDiscountedPrice(int itemCount)
+    {
+        try
+        {
+            int individualItems = itemCount;
+            int specialItemCount = 0;
+            if (this.SpecialCount > 0)
+            {
+                individualItems = itemCount % this.SpecialCount;
+                specialItemCount = (itemCount - individualItems) / this.SpecialCount;
+            }
+
+            return (individualItems * this.Price) + (specialItemCount * this.SpecialPrice);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            return 0;
+        }
+    }
+}
+
+public class Checkout : ICheckout
+{
+    public List<Item> Items;
+    public List<Item> ScannedItems;
+    public Checkout()
+    {
+        this.Items = new List<Item>();
+        this.ScannedItems = new List<Item>();
+    }
+
+    public void Scan(string item)
+    {
+        if (this.Items == null || this.Items.Count == 0)
+        {
+            return;
+        }
+
+        for (int i = 0; i < this.Items.Count; i++)
+        {
+            Item currentItem = this.Items[i];
+            if (currentItem != null && currentItem.Sku == item)
+            {
+                this.ScannedItems.Add(currentItem);
+                return;
+            }
+        }
+    }
+
+    public int GetTotal()
+    {
+        int totalPrice = 0;
+        if (this.ScannedItems == null || this.ScannedItems.Count == 0)
+        {
+            return totalPrice;
+        }
+
+        // Get the distinct SKUs from scanned items.   ====>>  ["A", "B", "C"]
+        List<Item> distinctSkus = this.getDistinctItems();
+        for (int i = 0; i < distinctSkus.Count; i++)
+        {
+            Item currentItem = distinctSkus[i];
+            int currentSkuCount = getCountForSku(currentItem.Sku);
+            totalPrice += currentItem.getDiscountedPrice(currentSkuCount);
+        }
+
+        return totalPrice;
+    }
+
+    public List<Item> getDistinctItems()
+    {
+        // initialise the list of distinct SKUs
+        List<Item> distinctItems = new List<Item>();
+        // null check, return empty list if invalid
+        if (this.ScannedItems == null || this.ScannedItems.Count == 0)
+        {
+            return distinctItems;
+        }
+
+        // iterate the list and insert distinct SKUs
+        for (int i = 0; i < this.ScannedItems.Count; i++)
+        {
+            Item scannedItem = this.ScannedItems[i];
+            if (!distinctItems.Contains(scannedItem))
+            {
+                distinctItems.Add(scannedItem);
+            }
+        }
+
+        return distinctItems;
+    }
+
+    public int getCountForSku(string sku)
+    {
+        int skuCount = 0;
+        // null check, return empty list if invalid
+        if (this.ScannedItems == null || this.ScannedItems.Count == 0)
+        {
+            return skuCount;
+        }
+
+        // iterate the list and insert distinct SKUs
+        for (int i = 0; i < this.ScannedItems.Count; i++)
+        {
+            Item scannedItem = this.ScannedItems[i];
+            if (scannedItem.Sku == sku)
+            {
+                ++skuCount;
+            }
+        }
+
+        return skuCount;
+    }
+
+    public void addItem(Item item)
+    {
+        this.Items.Add(item);
+    }
+}
